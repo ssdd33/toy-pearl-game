11c5b1d99b78674bbca1d5d0d598d5f5cd7355ce
ssdd33 <ojjonino@ojjoninoui-MacBookPro.local>
2020-12-18 11:15:53 +0900

    add button

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const $balls = document.querySelectorAll('.ball');
 const $ballrows = document.querySelectorAll('.ballrows');
 const $eyes = document.querySelectorAll('.pupil');
-
+const $start = document.querySelector('');
+const $
 document.onmousemove = function () {
-  let x = (event.clientX * 100) / window.innerwidth + '%'
-  let y = (event.clientY * 100) / window.innerwidth + '%'
+  let x = (event.clientX * 100) / window.innerWidth
+  let y = (event.clientY * 100) / window.innerHeight + '%'
   $eyes.forEach((eye) => {
     eye.style.left = x;
     eye.style.top = y;
@@ -37,7 +38,7 @@ $balls.forEach((ball) =>
 
 // system
 // if 첫 턴이면 just go 가능 (턴 넘기기)
-// 둘째턴부터는 반드시 하나이상 선택해야 턴 넘길 수 이
+// 둘째턴부터는 반드시 하나이상 선택해야 턴 넘길 수 있음
 // Array 안에 0이 있으면 지우고, 없으면 미동작
 // Array[i]를 건드리면 그 외 다른 Array는 클릭이 불가능
 // 모든 Array에 0이 없으면 gameover
@@ -47,5 +48,9 @@ const ballObject = [
   [0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0]
 ];
+
+const gameSystem = function (ballObject) {
+  for (let row = 0; row < 4; row++)
+}
 
 
